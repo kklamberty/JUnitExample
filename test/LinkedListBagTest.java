1bf9b3fcb6d8aec1ccada1a3cd9f03f73ccfbd86
kklamberty <lamberty@morris.umn.edu>
2020-01-24 18:38:09 -0600

    Add tests and remove main method and other extra code
    Ass tests for for clearing the contents and adding contents,
    remove main and print statements

--- test/LinkedListBagTest.java
+++ test/LinkedListBagTest.java
@@ -33,25 +33,20 @@ class LinkedListBagTest {
     }
 
     @BeforeEach
-    void setUpEach(){
+    void setUpEach() {
         /* Here, we give bigBag2 its initial value -
         it's a LinkedListBag of things of type Item */
         bigBag2 = new LinkedListBag<>();
 
-        /*I am going to add objects from the user defined type to the list*/
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(item1));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(pizza));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(lasagna));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(chicken));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(burrito));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(noodles));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(donut));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(cheetos));
-        System.out.println("\n");
-        /* toArray shows what is in the list in the form of an array */
-        bigBag2.toArray();
-        System.out.println("\n");
-
+        /* Add objects from the user defined type to the list*/
+        bigBag2.add(item1);
+        bigBag2.add(pizza);
+        bigBag2.add(lasagna);
+        bigBag2.add(chicken);
+        bigBag2.add(burrito);
+        bigBag2.add(noodles);
+        bigBag2.add(donut);
+        bigBag2.add(cheetos);
     }
 
     @Test
@@ -96,45 +91,22 @@ class LinkedListBagTest {
         assertEquals(7, bigBag2.getCurrentSize());
     }
 
-    public static void main(String[]args){
-
-        /* This creates the object using the constructor from the LinkedListBag class which
-        implements the bagDataStructure interface */
-        bagDataStructure<Item> bigBag2 = new LinkedListBag<>();
-
-        /*I am creating the objects from the user defined data type*/
-        Item item1 = new Item("Tv",20000);
-        Item pizza = new Item("pizza",500);
-        Item lasagna = new Item("lasagna",900);
-        Item chicken = new Item("chicken",500);
-        Item burrito = new Item("burrito",200);
-        Item noodles = new Item("noodles",300);
-        Item donut = new Item("donut",100);
-        Item cheetos = new Item("cheetos",150);
-
-        /*I am going to add objects from the user defined type to the list*/
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(item1));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(pizza));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(lasagna));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(chicken));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(burrito));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(noodles));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(donut));
-        System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(cheetos));
-        System.out.println("\n");
-        /* toArray shows what is in the list in the form of an array */
-        bigBag2.toArray();
-        System.out.println("\n");
-
-        /* This section will test the methods using bigBag2 */
-
+    @Test
+    @DisplayName("Clear all")
+    void testClearAll() {
+        assertEquals(8, bigBag2.getCurrentSize());
         bigBag2.clear();
-        System.out.println("The clear method was just used, but there is no return value so yeah");
+        assertEquals(0, bigBag2.getCurrentSize());
+    }
 
-
-
-
-
+    @Test
+    @DisplayName("Add item to bag")
+    void testAdd() {
+        bigBag2.clear();
+        Item egg = new Item("eggs", 189);
+        bigBag2.add(egg);
+        assertTrue(bigBag2.contains(egg));
+        assertEquals(1, bigBag2.getCurrentSize());
     }
 }
 
