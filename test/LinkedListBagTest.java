5219ad4ef92452c9ab72419b9caea86d9cc247c6
kklamberty <lamberty@morris.umn.edu>
2020-01-24 18:24:44 -0600

    Add test for removing a specific item
    Testing this method made me realize I wanted to have a BeforeEach as well as a BeforeAll.
    I moved some things from the BeforeAll to a BeforeEach, and I made things not static that
    were in the BeforeEach (along with that method). I'm not sure of best practices here,
    but it struck me that having the bag be static might not make much sense, while items being static seemed fine.

--- test/LinkedListBagTest.java
+++ test/LinkedListBagTest.java
@@ -1,4 +1,5 @@
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,7 +8,7 @@ class LinkedListBagTest {
 
     /* Since this bag of items will be used for several tests,
     we want to declare it outside of any particular test */
-    private static bagDataStructure<Item> bigBag2;
+    private bagDataStructure<Item> bigBag2;
 
     /* Creating the objects from the user defined data type*/
     private static Item item1;
@@ -20,19 +21,23 @@ class LinkedListBagTest {
     private static Item cheetos;
 
     @BeforeAll
-    static void setUp(){
+    static void setUp() {
+        item1 = new Item("Tv", 20000);
+        pizza = new Item("pizza", 500);
+        lasagna = new Item("lasagna", 900);
+        chicken = new Item("chicken", 500);
+        burrito = new Item("burrito", 200);
+        noodles = new Item("noodles", 300);
+        donut = new Item("donut", 100);
+        cheetos = new Item("cheetos", 150);
+    }
+
+    @BeforeEach
+    void setUpEach(){
         /* Here, we give bigBag2 its initial value -
         it's a LinkedListBag of things of type Item */
         bigBag2 = new LinkedListBag<>();
 
-        item1 = new Item("Tv",20000);
-        pizza = new Item("pizza",500);
-        lasagna = new Item("lasagna",900);
-        chicken = new Item("chicken",500);
-        burrito = new Item("burrito",200);
-        noodles = new Item("noodles",300);
-        donut = new Item("donut",100);
-        cheetos = new Item("cheetos",150);
         /*I am going to add objects from the user defined type to the list*/
         System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(item1));
         System.out.println("Here I am adding an object using the user defined type item = " + bigBag2.add(pizza));
@@ -50,33 +55,44 @@ class LinkedListBagTest {
     }
 
     @Test
-    @DisplayName("Should not be empty - bags with items are not empty")
+    @DisplayName("Bags with items are not empty")
     void testEmpty() {
         assertFalse(bigBag2.isEmpty());
     }
 
     @Test
-    @DisplayName("Should have 8")
+    @DisplayName("Bag has 8 items")
     void testGetCurrentSize() {
         assertEquals(8, bigBag2.getCurrentSize());
     }
 
     @Test
-    @DisplayName("Should contain specific item (pizza)")
+    @DisplayName("Contains specific item (pizza)")
     void testContains() {
         assertTrue(bigBag2.contains(pizza));
     }
 
     @Test
-    @DisplayName("Should count instances of a thing - 1 burrito")
+    @DisplayName("Count instances of a thing - 1 burrito")
     void testGetFrequency() {
         assertEquals(1, bigBag2.getFrequencyOf(burrito));
     }
 
     @Test
-    @DisplayName("Should remove a random thing")
+    @DisplayName("Remove random thing")
     void testRemoveRandom() {
+        assertEquals(8, bigBag2.getCurrentSize());
         bigBag2.removeRandom();
+        assertEquals(7, bigBag2.getCurrentSize());
+    }
+
+    @Test
+    @DisplayName("Remove specific thing")
+    void testRemoveSpecific() {
+        assertTrue(bigBag2.contains(item1));
+        assertEquals(8, bigBag2.getCurrentSize());
+        assertTrue(bigBag2.removeSpecific(item1));
+        assertFalse(bigBag2.contains(item1));
         assertEquals(7, bigBag2.getCurrentSize());
     }
 
